992ca145de1b122ecbe5227f79fff407403c7f5c
vishnu shaji <vishnushaji@intersmart.in>
2025-01-24 17:34:03 +0530

    merged with merlin

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -26,40 +26,15 @@ class FrontendController extends Controller
         return view('about');
     }
 
-<<<<<<< HEAD
     /**
      * Display the brides page.
-=======
-      /**
-     * Display the index page.
->>>>>>> e7019b4d7e3e85e880d6046cec9cdc9b866cb600
      *
      * @author [Your Name Here]
      * @return \Illuminate\View\View
      */
-<<<<<<< HEAD
     
     public function brides()
     {
         return view('brides');
     }
 }
-=======
-    public function categoryListing()
-    {
-        return view('categoryListing');
-    }
-
-    
-      /**
-     * Display the index page.
-     *
-     * @author [Your Name Here]
-     * @return \Illuminate\View\View
-     */
-    public function subCategoryListing()
-    {
-        return view('subCategoryListing');
-    }
-}
->>>>>>> e7019b4d7e3e85e880d6046cec9cdc9b866cb600
